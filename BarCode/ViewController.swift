767dbe5ad81a3e841bd5b15425b119d8f5c2c5c0
Fabian Romero Sotelo <astramex19@gmail.com>
2018-09-27 22:25:03 -0500

    Qr detection with plane ok

--- BarCode/ViewController.swift
+++ BarCode/ViewController.swift
@@ -11,7 +11,7 @@ import SceneKit
 import ARKit
 import Vision
 
-class ViewController: UIViewController, ARSCNViewDelegate{
+class ViewController: UIViewController, ARSCNViewDelegate,ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -19,27 +19,30 @@ class ViewController: UIViewController, ARSCNViewDelegate{
     var detectedDataAnchor: ARAnchor?
     var processing = false
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.session.delegate=self
+     
         startQrCodeDetection()
     }
+    
+  
     
     func startQrCodeDetection() {
         // Create a Barcode Detection Request
         let request = VNDetectBarcodesRequest(completionHandler: self.requestHandler)
         // Set it to recognize QR code only
-        request.symbologies = [.QR]
+        request.symbologies = [.EAN13]
         self.qrRequests = [request]
     }
     
     func requestHandler(request: VNRequest, error: Error?) {
         // Get the first result out of the results, if there are any
         if let results = request.results, let result = results.first as? VNBarcodeObservation {
-            guard let payload = result.payloadStringValue else {return}
+            guard result.payloadStringValue != nil else {return}
             // Get the bounding box for the bar code and find the center
             var rect = result.boundingBox
             // Flip coordinates
@@ -56,13 +59,13 @@ class ViewController: UIViewController, ARSCNViewDelegate{
             self.processing = false
         }
     }
-
+    
     func hitTestQrCode(center: CGPoint) {
         if let hitTestResults = sceneView?.hitTest(center, types: [.featurePoint] ),
             let hitTestResult = hitTestResults.first {
             if let detectedDataAnchor = self.detectedDataAnchor,
                 let node = self.sceneView.node(for: detectedDataAnchor) {
-                let previousQrPosition = node.position
+                _ = node.position
                 node.transform = SCNMatrix4(hitTestResult.worldTransform)
                 
             } else {
@@ -82,7 +85,14 @@ class ViewController: UIViewController, ARSCNViewDelegate{
         // Run the view's session
         sceneView.session.run(configuration)
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
 
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
     public func session(_ session: ARSession, didUpdate frame: ARFrame) {
         DispatchQueue.global(qos: .userInitiated).async {
             do {
@@ -102,15 +112,61 @@ class ViewController: UIViewController, ARSCNViewDelegate{
     }
 
     // MARK: - ARSCNViewDelegate
+    
+
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+
+        
         if self.detectedDataAnchor?.identifier == anchor.identifier {
-            let sphere = SCNSphere(radius: 1.0)
-            sphere.firstMaterial?.diffuse.contents = UIColor.red
-            let sphereNode = SCNNode(geometry: sphere)
-            sphereNode.transform = SCNMatrix4(anchor.transform)
-            return sphereNode
+            
+            let node = SCNNode()
+            let plane = SCNPlane(width: 0.5, height: 0.5)
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi/2
+            
+            
+            
+            
+            node.addChildNode(planeNode)
+           
+//            node.addChildNode(addView())
+            return node
+        
         }
+        
+        
+        
         return nil
+    }
+    
+    func addView() ->SCNNode{
+
+        
+        
+        let skScene = SKScene(size: CGSize(width: 200, height: 200))
+        skScene.backgroundColor = UIColor.clear
+        
+        let rectangle = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 200, height: 200), cornerRadius: 10)
+        rectangle.fillColor = #colorLiteral(red: 0.807843148708344, green: 0.0274509806185961, blue: 0.333333343267441, alpha: 1.0)
+        rectangle.strokeColor = #colorLiteral(red: 0.439215689897537, green: 0.0117647061124444, blue: 0.192156866192818, alpha: 1.0)
+        rectangle.lineWidth = 5
+        rectangle.alpha = 0.4
+        let labelNode = SKLabelNode(text: "Hello World")
+        labelNode.fontSize = 20
+        labelNode.fontName = "Arial"
+        labelNode.position = CGPoint(x:0,y:0)
+        skScene.addChild(rectangle)
+        skScene.addChild(labelNode)
+        
+        
+        let plane = SCNPlane(width: 0.20, height: 0.20)
+        let material = SCNMaterial()
+        material.isDoubleSided = true
+        material.diffuse.contents = skScene
+        plane.materials = [material]
+        let planeNode = SCNNode(geometry: plane)
+
+        return planeNode
     }
 }
